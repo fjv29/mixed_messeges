58b72a09ec53b3ba0c6e01212111fd7f0f8a08f6
Francisco Vargas <sisco429@icloud.com>
2024-03-11 06:52:02 -0400

    First code for ingredient generator

--- project.js
+++ project.js
@@ -1,1 +1,10 @@
-console.log('start');
+const ingredients = ['banana', 'blueberry', 'apple', 'strawberry', 'pineapple', 'orange', 'kiwi',  ];
+
+const getIngredients = () =>{
+    let ingredient1 = ingredients[Math.floor(Math.random()* ingredients.length)];
+    let ingredient2 = ingredients[Math.floor(Math.random()* ingredients.length)];
+    let ingredient3 = ingredients[Math.floor(Math.random()* ingredients.length)];
+    console.log(ingredient1, ingredient2, ingredient3);
+}
+
+getIngredients();
