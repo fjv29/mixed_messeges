08e04f40d313e9ed5c57db79fc9be79ead5f1878
Francisco Vargas <sisco429@icloud.com>
2024-03-11 18:12:18 -0400

    Project was changed to random fortune generator

--- project.js
+++ project.js
@@ -1,10 +1,11 @@
-const ingredients = ['banana', 'blueberry', 'apple', 'strawberry', 'pineapple', 'orange', 'kiwi',  ];
+const wealth = ['be richer than Elon Musk.','be richer than Elon Musk.', 'be a millionaire.','be a millionaire.', 'end up with just enough money to retire.', 'will die with no money.', 'will be poor your entire life.','be broke', 'will die with no money.', 'will be poor your entire life.','be broke', 'will die with no money.', 'will be poor your entire life.','be broke', 'will die with no money.', 'will be poor your entire life.','be broke', 'will die with no money.', 'will be poor your entire life.','be broke', 'will die with no money.', 'will be poor your entire life.','be broke'];
+const love = ['meet the love of your life and live happily ever after.', 'meet a great partner and live a happy life.', 'die alone.', 'have lots of trouble finding love.','meet the love of your life and live happily ever after.', 'meet a great partner and live a happy life.', 'die alone.', 'have lots of trouble finding love.','meet the love of your life and live happily ever after.', 'meet a great partner and live a happy life.', 'die alone.', 'have lots of trouble finding love.','meet the love of your life and live happily ever after.', 'meet a great partner and live a happy life.', 'die alone.', 'have lots of trouble finding love.'];
 
-const getIngredients = () =>{
-    let ingredient1 = ingredients[Math.floor(Math.random()* ingredients.length)];
-    let ingredient2 = ingredients[Math.floor(Math.random()* ingredients.length)];
-    let ingredient3 = ingredients[Math.floor(Math.random()* ingredients.length)];
-    console.log(ingredient1, ingredient2, ingredient3);
-}
+const predictFuture = () => {
+    let wealthStatus = wealth[Math.floor(Math.random()* wealth.length)];
+    let loveStatus = love[Math.floor(Math.random()* love.length)];
+    let deathAge = [Math.floor(Math.random()* 50) + 40];
+    console.log(`I will tell you your fortune. To start off financially you will ${wealthStatus} When it comes to your love life you will ${loveStatus} Last you will live until you are ${deathAge}.`)
+};
 
-getIngredients();
+predictFuture();
